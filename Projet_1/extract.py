7e9272d61d36625f066f3735e2542eb127f0686a
Martin Crochelet <martin.crochelet@gmail.com>
2014-02-19 09:49:32 +0100

    tab replacement

--- Projet_1/extract.py
+++ Projet_1/extract.py
@@ -11,69 +11,69 @@ number = re.compile('\d+')
 
 
 class Transcript_gen:
-	def __init__(self, file):
-		self.fo = open(file, 'r', encoding='utf-16')
-		self.tran = ''
-		self.line = self.fo.readline()
-	
-	# not used, does not terminate loop correctly
-	def extract(self):
-		while self.line != '' :
-			while self.line != '<transcript_end>\n' :
-				self.tran += self.line
-				self.line = self.fo.readline()
-			yield self.tran
-			self.tran = ''
-			self.line = self.fo.readline()
-		print('doneloop')
+    def __init__(self, file):
+        self.fo = open(file, 'r', encoding='utf-16')
+        self.tran = ''
+        self.line = self.fo.readline()
+    
+    # not used, does not terminate loop correctly
+    def extract(self):
+        while self.line != '' :
+            while self.line != '<transcript_end>\n' :
+                self.tran += self.line
+                self.line = self.fo.readline()
+            yield self.tran
+            self.tran = ''
+            self.line = self.fo.readline()
+        print('doneloop')
 
-	# Returns next transcript
-	def getnext(self):
-		ntran = ''
-		while (self.line!='<transcript_end>\n' and self.line != ''):
-			ntran += self.line
-			self.line = self.fo.readline()
-		self.line = self.fo.readline()
-		return ntran
+    # Returns next transcript
+    def getnext(self):
+        ntran = ''
+        while (self.line!='<transcript_end>\n' and self.line != ''):
+            ntran += self.line
+            self.line = self.fo.readline()
+        self.line = self.fo.readline()
+        return ntran
 
 # Supposes the first mentionned age is most likely to be the patient's
 # does not cover fully typed out numbers (e.g. 'two-and-a-half-year-old')
 def get_age(s):
-	matches = []
-	for re in age:
-		m = re.search(s)
-		if m != None:
-			matches += [(m.start(), m)]
-	matches.sort()
-	if matches != []:
-		first = matches[0][1].group()
-		num = number.search(matches[0][1].group()).group()
-		if days.search(first) != None :
-			return num+' d'
-		elif months.search(first) != None:
-			return num + ' m'
-		elif weeks.search(first) != None:
-			return num + ' w'
-		else:
-			return num + ' y'
-	else:
-		return 'none'
+    matches = []
+    for re in age:
+        m = re.search(s)
+        if m != None:
+            matches += [(m.start(), m)]
+    matches.sort()
+    if matches != []:
+        first = matches[0][1].group()
+        num = number.search(matches[0][1].group()).group()
+        if days.search(first) != None :
+            return num+' d'
+        elif months.search(first) != None:
+            return num + ' m'
+        elif weeks.search(first) != None:
+            return num + ' w'
+        else:
+            return num + ' y'
+    else:
+        return 'none'
 
 
 def get_gender(s):
-	m = male.search(s)
-	f = female.search(s)
-	if m == None:
-		if f == None:
-			return '-'
-		else:
-			return 'female'
-	else:
-		return 'male'
+    m = male.search(s)
+    f = female.search(s)
+    if m == None:
+        if f == None:
+            return '-'
+        else:
+            return 'female'
+    else:
+        return 'male'
 
 
 p = Transcript_gen('medical_transcripts.txt')
 s = p.getnext()
 while s != '':
-	print(get_age(s)+ ' ' + get_gender(s))
-	s= p.getnext()
+    print(get_age(s)+ ' ' + get_gender(s))
+    s= p.getnext()
